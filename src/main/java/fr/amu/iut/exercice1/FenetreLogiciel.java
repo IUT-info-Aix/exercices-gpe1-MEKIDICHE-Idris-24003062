1731c85bba9d13c991713e813b61c3a5ca86d763
Idris <idris.mekidiche@etu.univ-amu.fr>
2025-04-23 22:16:14 +0200

    termine le centre de l'exercice 1

--- src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
+++ src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
@@ -1,12 +1,10 @@
 package fr.amu.iut.exercice1;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 
@@ -19,28 +17,41 @@ public class FenetreLogiciel extends Application {
 
         // Création du conteneur correspondant à la ligne de contrôle haut dessus du tableau
         Menu menu1 = new Menu("File");
-        Menu menu2 = new Menu("Options");
+        Menu menu2 = new Menu("Edit");
         Menu menu3 = new Menu("Help");
         MenuBar menuBar = new MenuBar(menu1, menu2, menu3);
         root.setTop(menuBar);
 
+        //la gauche
         VBox lesbutton = new VBox();
-        Label entrer = new Label("Bouton : ");
+        lesbutton.setAlignment(Pos.CENTER);
+        lesbutton.setSpacing(5);
+        Label entrer = new Label("Boutons : ");
         Button buton1 = new Button("Bouton 1");
         Button buton2 = new Button("Bouton 2");
         Button buton3 = new Button("Bouton 3");
         lesbutton.getChildren().addAll(entrer,buton1,buton2,buton3);
         root.setLeft(lesbutton);
 
+        //le centre
         GridPane gridpane = new GridPane();
-        gridpane.add(new Label("Nom"), 0, 1);
-        gridpane.add(new Label("Email"), 0, 2);
-        gridpane.add(new Label("Password"), 0, 3);
+        gridpane.setAlignment(Pos.CENTER);
+        gridpane.setHgap(5);
+        gridpane.add(new Label("Name:"), 0, 1);
+        TextField champ = new TextField();
+        champ.setMaxWidth(260);
+        gridpane.add(champ, 1, 1);
+        gridpane.add(new Label("Email:"), 0, 2);
+        TextField champ2 = new TextField();
+        champ.setMaxWidth(260);
+        gridpane.add(champ2, 1, 2);
+        gridpane.add(new Label("Password:"), 0, 3);
+        TextField champ3 = new TextField();
+        champ.setMaxWidth(260);
+        gridpane.add(champ3, 1, 3);
         gridpane.add(new Button("Submit"), 0, 4);
         gridpane.add(new Button("Cancel"), 1, 4);
         root.setCenter(gridpane);
-
-
 
         // Création de la ligne de séparation
         Separator sep = new Separator();
@@ -49,9 +60,17 @@ public class FenetreLogiciel extends Application {
 
         // Ajout de la scene à la fenêtre et changement de ses paramètres (dimensions et titre)
         primaryStage.setScene( scene );
-        primaryStage.setWidth( 500 );
-        primaryStage.setHeight( 400 );
+        primaryStage.setWidth( 600 );
+        primaryStage.setHeight( 500 );
         primaryStage.setTitle("VBox and HBox App");
+
+        //le bas
+        Label bottom = new Label("Ceci est un label de bas de page");
+        HBox bottomBox = new HBox();
+        bottomBox.setAlignment(Pos.CENTER);
+        bottomBox.getChildren().add(bottom);
+        root.setBottom(bottomBox);
+
 
         // Affichage de la fenêtre
         primaryStage.show();
