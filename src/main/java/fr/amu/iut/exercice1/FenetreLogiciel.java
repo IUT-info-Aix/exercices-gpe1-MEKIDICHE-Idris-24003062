5560c890d890b57c0f3b3ee7d5c5e77ac1969382
Idris <idris.mekidiche@etu.univ-amu.fr>
2025-04-24 22:43:34 +0200

    preque terminer la fin de l'exercice 1

--- src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
+++ src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
@@ -1,12 +1,15 @@
 package fr.amu.iut.exercice1;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+import static java.awt.SystemColor.menu;
 
 public class FenetreLogiciel extends Application {
     @Override
@@ -15,70 +18,86 @@ public class FenetreLogiciel extends Application {
         // Création du conteneur principal
         BorderPane root = new BorderPane();
 
-        // Création du conteneur correspondant à la ligne de contrôle haut dessus du tableau
+        // Création du Menu "File" avec les sous-items
         Menu menu1 = new Menu("File");
+        MenuItem newItem = new MenuItem("New");
+        MenuItem openItem = new MenuItem("Open");
+        MenuItem saveItem = new MenuItem("Save");
+        MenuItem closeItem = new MenuItem("Close");
+        menu1.getItems().addAll(newItem, openItem, saveItem, closeItem);
+
         Menu menu2 = new Menu("Edit");
+        MenuItem cutItem = new MenuItem("Cut");
+        MenuItem copyItem = new MenuItem("Copy");
+        MenuItem pasteItem = new MenuItem("Paste");
+        menu2.getItems().addAll(cutItem, copyItem, pasteItem);
+
         Menu menu3 = new Menu("Help");
         MenuBar menuBar = new MenuBar(menu1, menu2, menu3);
         root.setTop(menuBar);
 
-        //la gauche
+        // Création des boutons à gauche
         VBox lesbutton = new VBox();
         lesbutton.setAlignment(Pos.CENTER);
-        lesbutton.setSpacing(5);
+        lesbutton.setSpacing(6);
         Label entrer = new Label("Boutons : ");
         Button buton1 = new Button("Bouton 1");
         Button buton2 = new Button("Bouton 2");
         Button buton3 = new Button("Bouton 3");
-        lesbutton.getChildren().addAll(entrer,buton1,buton2,buton3);
+
+        lesbutton.getChildren().addAll(entrer, buton1, buton2, buton3);
         root.setLeft(lesbutton);
 
-        //le centre
+        // Créer le GridPane pour le centre avec un alignement centré
         GridPane gridpane = new GridPane();
-        gridpane.setAlignment(Pos.CENTER);
-        gridpane.setHgap(5);
+        gridpane.setAlignment(Pos.CENTER); // Centrer le GridPane dans la fenêtre
         gridpane.add(new Label("Name:"), 0, 1);
         TextField champ = new TextField();
         champ.setMaxWidth(260);
         gridpane.add(champ, 1, 1);
         gridpane.add(new Label("Email:"), 0, 2);
         TextField champ2 = new TextField();
-        champ.setMaxWidth(260);
+        champ2.setMaxWidth(260);
         gridpane.add(champ2, 1, 2);
         gridpane.add(new Label("Password:"), 0, 3);
         TextField champ3 = new TextField();
-        champ.setMaxWidth(260);
+        champ3.setMaxWidth(260);
         gridpane.add(champ3, 1, 3);
         gridpane.add(new Button("Submit"), 0, 4);
         gridpane.add(new Button("Cancel"), 1, 4);
-        root.setCenter(gridpane);
+        Separator sepVert = new Separator(Orientation.VERTICAL);
+        sepVert.setPadding(new Insets(0, 10, 0, 10));
 
-        // Création de la ligne de séparation
-        Separator sep = new Separator();
-        // Ajout du conteneur à la scene
+        // HBox combinant la colonne de boutons et le formulaire
+        HBox hBoxCenter = new HBox(lesbutton, sepVert, gridpane);
+        hBoxCenter.setAlignment(Pos.BOTTOM_CENTER);
+        hBoxCenter.setSpacing(10);
+
+        root.setCenter(hBoxCenter);
+
+        // Création de la scène
         Scene scene = new Scene(root);
 
-        // Ajout de la scene à la fenêtre et changement de ses paramètres (dimensions et titre)
-        primaryStage.setScene( scene );
-        primaryStage.setWidth( 600 );
-        primaryStage.setHeight( 500 );
-        primaryStage.setTitle("VBox and HBox App");
-
-        //le bas
-        Label bottom = new Label("Ceci est un label de bas de page");
-        HBox bottomBox = new HBox();
-        bottomBox.setAlignment(Pos.CENTER);
-        bottomBox.getChildren().add(bottom);
-        root.setBottom(bottomBox);
-
-
-        // Affichage de la fenêtre
-        primaryStage.show();
+        // Paramètres de la fenêtre
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(700);
+        primaryStage.setHeight(600);
         primaryStage.setTitle("Premier exemple manipulant les conteneurs");
+
+        // Le bas
+        Separator sepHoriz = new Separator(Orientation.HORIZONTAL);
+        Label bottomLabel = new Label("Ceci est un label de bas de page");
+        HBox bottomBox = new HBox(bottomLabel);
+        bottomBox.setAlignment(Pos.CENTER);
+        bottomBox.setPadding(new Insets(10));
+
+        // On met le séparateur et le label dans une VBox, puis on le place en bas
+        VBox bottomContainer = new VBox(sepHoriz, bottomBox);
+        root.setBottom(bottomContainer);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 }
-
